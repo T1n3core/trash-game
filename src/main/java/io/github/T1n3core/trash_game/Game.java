6d6683caaf4fc8226c9d477b594dfb9a8b5af2ee
Tine Stakul <tine.stakul@gmail.com>
2026-03-06 13:20:26 +0100

    class hierarchy established

---
+++ src/main/java/io/github/T1n3core/trash_game/Game.java
@@ -0,0 +1,7 @@
+package io.github.T1n3core.trash_game;
+
+import javax.swing.JFrame;
+
+public class Game extends JFrame implements Runnable {
+    
+}
---
+++ src/main/java/io/github/T1n3core/trash_game/GamePanel.java
@@ -0,0 +1,8 @@
+package io.github.T1n3core.trash_game;
+
+import java.awt.event.KeyListener;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements KeyListener {
+    
+}
