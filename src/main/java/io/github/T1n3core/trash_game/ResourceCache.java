590d539e3aee2bc1a933e5d88a3cb50b2b9f3cd7
Tine Stakul <tine.stakul@gmail.com>
2026-03-11 15:24:38 +0100

    load resources from classpath now

--- src/main/java/io/github/T1n3core/trash_game/ResourceCache.java
+++ src/main/java/io/github/T1n3core/trash_game/ResourceCache.java
@@ -1,11 +1,11 @@
 package io.github.T1n3core.trash_game;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public final class ResourceCache {
+
     public static final BufferedImage PLAYER;
     public static final BufferedImage BIO;
     public static final BufferedImage PAPER;
@@ -16,16 +16,20 @@ public final class ResourceCache {
 
     static {
         try {
-            PLAYER = ImageIO.read(new File("player.png"));
-            BIO = ImageIO.read(new File("bio.png"));
-            PAPER = ImageIO.read(new File("paper.png"));
-            PLASTIC = ImageIO.read(new File("plastic.png"));
-            PLANE = ImageIO.read(new File("plane.png"));
-            SEED = ImageIO.read(new File("seed.png"));
-            BACKGROUND = ImageIO.read(new File("background.png"));
+            PLAYER = load("player.png");
+            BIO = load("bio.png");
+            PAPER = load("paper.png");
+            PLASTIC = load("plastic.png");
+            PLANE = load("plane.png");
+            SEED = load("seed.png");
+            BACKGROUND = load("background.png");
         } catch (IOException e) {
             throw new RuntimeException("Failed to load resources", e);
         }
+    }
+
+    private static BufferedImage load(String name) throws IOException {
+        return ImageIO.read(ResourceCache.class.getResourceAsStream("/" + name));
     }
 
     private ResourceCache() {}
